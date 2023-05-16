45bd9b98e11190a3640c4dcc5502a8c8c6f6063f
ibrahimYesiloglu <cizgi.2d@gmail.com>
2023-05-16 19:34:25 +0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,43 @@
+fun main(args: Array<String>) {
+   /* val name: String? =null
+    println("Ibrahim Yesiloglu")
+    println(name)
+    *//*******************************//*
+    val name2: String = "Haci"
+    println(name2!!.length)
+    *//*******************************//*
+    val name3: String = "Bacio"
+    println(name3!!.uppercase())*/
+    /*******************************/
+/*
+ sayHallo()
+    sayHallo2("Jonn")
+    val yas = sayHallo3("Coni")
+    println("yasiniz $yas")
+    val sayi=randomNummer()
+    println(sayi)
+}
+fun  sayHallo(){
+    println("Hallo")
+}
+fun sayHallo2(name: String){
+    println("Hello $name")
+}
+fun sayHallo3(name:String): Int{
+    return name.length
+}
+fun randomNummer():Int{
+    return (0..10).random() */
+    println(topla(5,8))
+    println(topla2(43,58))
+println(" ${isEmpty( "")}")
+}  /***********************************************/
+
+fun topla (a:Int,b:Int):Int {
+    return a+b
+}
+fun topla2 (c:Int,d:Int)=c+d
+
+fun isEmpty(str:String)=str.isEmpty()
+
+
